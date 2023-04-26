4a1bf91d5b2d31102519e462be69325c0d153313
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2023-04-26 08:28:38 +0600

    Add files via upload

---
+++ temporary.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main()
+{
+	int i, j = 1;
+	for (i = 0; i < 7; i++)
+	{
+		if (i % 2 != 0)
+		{
+			j -= 2;
+			printf("%3d\n", j);
+			j += 2;
+		}
+		else
+		{
+			printf("%3d\n", j);
+		}
+	}
+
+	return 0;
+}
